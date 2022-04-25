a81a8b37ea0393d5342997ddc6b2b226ed2deee3
menjivar98 <00195316@uca.edu.sv>
2022-04-25 13:46:32 -0600

    Ya da como resultado en la direccion 210h perro en la 200

--- add.asm
+++ add.asm
@@ -3,13 +3,23 @@
         section .text
 
 ; suma usando registros
-        mov     AL, 8d
-        mov     BL, 6d
-        add     AL, BL
+        mov     AH, 0d
+        mov     AL, 0d
+        mov     BH, 1d
+        mov     BL, 9d
+        mov     CH, 5d
+        mov     CL, 3d
+        mov     DH, 1d
+        mov     DL, 6d
+
+        add     CL, DH
+        add     CL, DL
+        
+       
 
 ; suma usando celda de memoria
 ; cuando la fuente es un dato inmediato y el destino es una celda de memoria, es necesario indicar el tamaño con una directiva
-        mov     byte[200h], AL
-        add     byte[200h], BL
+        mov     byte[200h], AH
+        mov     byte[210h], CL       
 
         int     20h
